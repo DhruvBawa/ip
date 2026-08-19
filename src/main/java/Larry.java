a09a17e3f0856c016cc695b15471ca5f8dfd1bb6
DhruvBawa <dhruvbawa3@gmail.com>
2026-08-19 21:08:29 +0800

    Level0 finished

---
+++ src/main/java/Larry.java
@@ -0,0 +1,14 @@
+/**
+ * Runs the Larry chatbot application.
+ */
+public class Larry {
+    public static void main(String[] args) {
+        String banner = "██╗      █████╗ ██████╗ ██████╗ ██╗   ██╗\n"
+                + "██║     ██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝\n"
+                + "██║     ███████║██████╔╝██████╔╝ ╚████╔╝ \n"
+                + "██║     ██╔══██║██╔══██╗██╔══██╗  ╚██╔╝  \n"
+                + "███████╗██║  ██║██║  ██║██║  ██║   ██║   \n"
+                + "╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   \n";
+        System.out.println(banner);
+    }
+}
