076a41be31fad8ae13b13b7e2f389e1663f446f2
DhruvBawa <dhruvbawa3@gmail.com>
2026-08-19 21:23:07 +0800

    Level0 b) done

--- src/main/java/Larry.java
+++ src/main/java/Larry.java
@@ -3,12 +3,20 @@
  */
 public class Larry {
     public static void main(String[] args) {
-        String banner = "██╗      █████╗ ██████╗ ██████╗ ██╗   ██╗\n"
-                + "██║     ██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝\n"
-                + "██║     ███████║██████╔╝██████╔╝ ╚████╔╝ \n"
-                + "██║     ██╔══██║██╔══██╗██╔══██╗  ╚██╔╝  \n"
-                + "███████╗██║  ██║██║  ██║██║  ██║   ██║   \n"
-                + "╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   \n";
+      String seperator = "________________________________________________________________";
+      System.out.println(seperator + "\n");
+
+      String banner = "██╗      █████╗ ██████╗ ██████╗ ██╗   ██╗\n"
+                    + "██║     ██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝\n"
+                    + "██║     ███████║██████╔╝██████╔╝ ╚████╔╝ \n"
+                    + "██║     ██╔══██║██╔══██╗██╔══██╗  ╚██╔╝  \n"
+                    + "███████╗██║  ██║██║  ██║██║  ██║   ██║   \n"
+                    + "╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   \n";
         System.out.println(banner);
+        
+      System.out.println("Hello! I'm Larry.\nWhat can I do for you?");
+      System.out.println(seperator);
+      System.out.println("Bye. Hope to see you again soon!");
+      System.out.println(seperator);
     }
 }
