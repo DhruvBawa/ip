ba860364143754a832282af715de9ae9b712f025
DhruvBawa <dhruvbawa3@gmail.com>
2026-08-20 22:56:56 +0800

    Level-4 attempt1

--- src/main/java/Larry.java
+++ src/main/java/Larry.java
@@ -15,19 +15,19 @@ public class Larry {
     public static void main(String[] args) {
         System.out.println(SEPARATOR + "\n");
 
-        String banner = "██╗      █████╗ ██████╗ ██████╗ ██╗   ██╗\n"
-                      + "██║     ██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝\n"
-                      + "██║     ███████║██████╔╝██████╔╝ ╚████╔╝ \n"
-                      + "██║     ██╔══██║██╔══██╗██╔══██╗  ╚██╔╝  \n"
-                      + "███████╗██║  ██║██║  ██║██║  ██║   ██║   \n"
-                      + "╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   \n";
+        String banner = "██╗      █████╗ ██████╗ ██████╗ ██╗   ██╗      ██╗                   ╔██╗\n"
+                      + "██║     ██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝      ████╗               ╔████║\n"
+                      + "██║     ███████║██████╔╝██████╔╝ ╚████╔╝       █████████████████████████║\n"
+                      + "██║     ██╔══██║██╔══██╗██╔══██╗  ╚██╔╝        ██║    ██        ██    ██║\n"
+                      + "███████╗██║  ██║██║  ██║██║  ██║   ██║         ██║         ▲          ██║\n"
+                      + "╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝         ██║       ╚═══╝        ██║\n"
+                      + "                                               ╚════════════════════════╝\n";
         System.out.println(banner);
 
         System.out.println("Hello! I'm Larry.\nWhat can I do for you?");
         System.out.println(SEPARATOR);
 
-        String[] tasks = new String[MAX_TASKS];
-        boolean[] taskDone = new boolean[MAX_TASKS];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
@@ -43,23 +43,40 @@ public class Larry {
             if (command.equals("list")) {
                 System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    String status = taskDone[i] ? "X" : " ";
-                    System.out.println("     " + (i + 1) + ".[" + status + "] " + tasks[i]);
+                    System.out.println("     " + (i + 1) + "." + tasks[i]);
                 }
             } else if (command.startsWith("mark ")) {
                 int taskIndex = Integer.parseInt(command.substring(5)) - 1;
-                taskDone[taskIndex] = true;
+                tasks[taskIndex].markAsDone();
                 System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       [X] " + tasks[taskIndex]);
+                System.out.println("       " + tasks[taskIndex]);
             } else if (command.startsWith("unmark ")) {
                 int taskIndex = Integer.parseInt(command.substring(7)) - 1;
-                taskDone[taskIndex] = false;
+                tasks[taskIndex].markAsNotDone();
                 System.out.println("     OK, I've marked this task as not done yet:");
-                System.out.println("       [ ] " + tasks[taskIndex]);
+                System.out.println("       " + tasks[taskIndex]);
             } else {
-                tasks[taskCount] = command;
+                Task newTask;
+                if (command.startsWith("todo ")) {
+                    String description = command.substring(5);
+                    newTask = new Todo(description);
+                } else if (command.startsWith("deadline ")) {
+                    String[] parts = command.substring(9).split(" /by ", 2);
+                    newTask = new Deadline(parts[0], parts[1]);
+                } else if (command.startsWith("event ")) {
+                    String[] descriptionAndTimes = command.substring(6).split(" /from ", 2);
+                    String[] startAndEnd = descriptionAndTimes[1].split(" /to ", 2);
+                    newTask = new Event(descriptionAndTimes[0], startAndEnd[0], startAndEnd[1]);
+                } else {
+                    newTask = new Task(command);
+                }
+
+                tasks[taskCount] = newTask;
                 taskCount++;
-                System.out.println("     added: " + command);
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + newTask);
+                String taskWord = taskCount == 1 ? "task" : "tasks";
+                System.out.println("     Now you have " + taskCount + " " + taskWord + " in the list.");
             }
 
             System.out.println(SEPARATOR);
