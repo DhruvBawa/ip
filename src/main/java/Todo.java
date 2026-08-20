8403f0ab4d9adc42983e3bc3ab8ee6cfe5637a1a
DhruvBawa <dhruvbawa3@gmail.com>
2026-08-20 22:56:56 +0800

    Level-4 attempt1

---
+++ src/main/java/Todo.java
@@ -0,0 +1,23 @@
+/**
+ * Represents a task without a date or time.
+ */
+public class Todo extends Task {
+    /**
+     * Creates a todo task with the given description.
+     *
+     * @param description description of the task
+     */
+    public Todo(String description) {
+        super(description);
+    }
+
+    /**
+     * Returns the task in the format used by Larry's responses.
+     *
+     * @return status icon followed by the task description
+     */
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
