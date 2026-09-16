fef3ca6119d3b90767c2e0ac5c9197a6135c2ae5
DhruvBawa <dhruvbawa3@gmail.com>
2026-09-16 14:21:22 +0800

    Add task storage and numbered listing
    Echoing input does not retain tasks for later review. Store up to 100
    task descriptions in memory and display them in order with list.
    
    Reconstruct Level-2 from the combined Level-3 commit dd5b3e4.

--- src/main/java/Larry.java
+++ src/main/java/Larry.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
  */
 public class Larry {
     private static final String SEPARATOR = "    __________________________________________________________";
+    private static final int MAX_TASKS = 100;
 
     /**
-     * Starts Larry and echoes commands until the user enters {@code bye}.
+     * Starts Larry and responds to commands until the user enters {@code bye}.
      *
      * @param args Command-line arguments; not used.
      */
@@ -25,6 +26,8 @@ public class Larry {
         System.out.println("Hello! I'm Larry.\nWhat can I do for you?");
         System.out.println(SEPARATOR);
 
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -36,7 +39,17 @@ public class Larry {
                 break;
             }
 
-            System.out.println("     " + command);
+            if (command.equals("list")) {
+                System.out.println("     Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("     added: " + command);
+            }
+
             System.out.println(SEPARATOR);
         }
         scanner.close();
